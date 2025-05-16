c36355514e058c3f322aa6b406cdb11b0876d555
lokr314 <louiskrueger2002@gmail.com>
2025-05-16 16:20:57 +0200

    Add Test, extend gitignore

---
+++ test/test_main.cpp
@@ -0,0 +1,35 @@
+//
+// Created by lokr on 5/16/25.
+//
+#include <iostream>
+#include "QCircuit.hpp"
+
+int main() {
+
+  // Test the readQASM function with the "test/3_17_14.qasm" file
+  qcore::QCircuit circuit;
+  std::ifstream qasm_file("test/3_17_14.qasm");
+  if (!qasm_file.is_open()) {
+    std::cerr << "Error opening file!" << std::endl;
+    return 1;
+  }
+  circuit.readQASM(qasm_file);
+  qasm_file.close();
+  // Test the writeQASM function
+  std::ofstream output_file("test/output.qasm");
+  if (!output_file.is_open()) {
+    std::cerr << "Error opening output file!" << std::endl;
+    return 1;
+  }
+  circuit.writeQASM(output_file);
+  output_file.close();
+
+  if (1 == 1) {
+    std::cout << "Test Passed!\n";
+  }
+  else {
+    std::cout << "Test Failed!\n";
+    return 1;
+  }
+  return 0;
+}
